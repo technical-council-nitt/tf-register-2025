e23ed03d380314cf7028a2d55ac0c632f4b27d6f
Arunesh Unnithan <aruneshunnithan@gmail.com>
2024-10-07 18:11:18 +0530

    Something Something

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -100,7 +100,7 @@ const Dashboard = () => {
 
   return (
     <div className="flex flex-col min-h-screen bg-black text-white">
-      <nav className="flex justify-between items-center p-4 md:p-6 border-b-[1px] border-neutral-800">
+      <nav className="flex lg:max-h-[64px] justify-between items-center p-4 md:p-6 border-b-[1px] border-neutral-800">
         <img src="/motif.svg" alt="Logo" style={{ width: '40px', aspectRatio: '63 / 29' }} className="md:hidden block" />
         <img src="/motif-desk.svg" alt="Logo" style={{ width: '120px', aspectRatio: '155 / 20' }} className="md:block hidden" />
         <TooltipProvider>
@@ -120,80 +120,90 @@ const Dashboard = () => {
         </TooltipProvider>
       </nav>
 
-      <main className="flex-grow flex flex-col lg:flex-row-reverse lg:justify-between items-center lg:items-start px-4 md:px-6 pt-8">
-        <div
-          className="lg:w-[40%] md:w-1/2 w-full"
-          style={{
-            backgroundImage: `url('/team-card.svg')`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            aspectRatio: '343 / 216',
-            position: "relative",
-          }}
-        >
-          <h2 className="text-[1.4em] font-geist absolute bottom-0 left-0 p-[1em] md:p-[1.6em]">{team.name}</h2>
-          <div className="absolute bottom-0 right-0 p-6 pb-4">
-            {(team.payment_status === "Pending" || team.payment_status === "Failed") &&
-              <Button
-                className="bg-white text-black rounded-[120px] font-bold hover:bg-gray-100 transition duration-300 flex items-center justify-center gap-2"
-                onClick={() => navigate(`/${team.team_id}/pay`)}
-              >
-                <img src="/pay.svg" />
-              </Button>}
+      <div className="flex-grow flex flex-col justify-between h-full">
+        <div className="flex-grow flex flex-col lg:flex-row-reverse lg:justify-between items-center lg:items-start px-4 md:px-6 pt-8">
+          <div
+            className="lg:w-[40%] md:w-1/2 w-full"
+            style={{
+              backgroundImage: `url('/team-card.svg')`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              aspectRatio: '343 / 216',
+              position: "relative",
+            }}
+          >
+            <h2 className="text-[1.4em] font-geist absolute bottom-0 left-0 p-[1em] md:p-[1.6em] max-w-[50%]">{team.name}</h2>
+            <div className="absolute bottom-0 right-0 p-6 pb-4">
+              {(team.payment_status === "Pending" || team.payment_status === "Failed") && team.members.length >= 4 && team.members.length <= 5 &&
+                <Button
+                  className="bg-white text-black rounded-[120px] font-bold hover:bg-gray-100 transition duration-300 flex items-center justify-center gap-2"
+                  onClick={() => navigate(`/${team.team_id}/pay`)}
+                >
+                  <img src="/pay.svg" />
+                </Button>}
+            </div>
           </div>
-        </div>
-        <div className="lg:w-[60%] lg:pr-8">
-          <div className="w-full pt-3 mt-4 flex flex-col">
-            <h1 className="font-spacegrotesk text-3xl lg:text-5xl font-medium">Dashboard</h1>
-            <span className="text-neutral-500 pt-2">Invite your teammates by sharing the team ID. </span>
-          </div>
-          <div className="w-full space-y-6 pt-8">
-            {alertMessage && (
-              <Alert className="bg-green-500 text-white p-4 rounded-lg">
-                <AlertDescription>{alertMessage}</AlertDescription>
-              </Alert>
-            )}
+          <div className="lg:w-[60%] lg:pr-8">
+            <div className="w-full pt-3 mt-4 flex flex-col">
+              <h1 className="font-spacegrotesk text-3xl lg:text-5xl font-medium">Dashboard</h1>
+              <span className="text-neutral-500 pt-2">Invite your teammates by sharing the team ID. </span>
+            </div>
+            <div className="w-full space-y-6 pt-8">
+              {alertMessage && (
+                <Alert className="bg-green-500 text-white p-4 rounded-lg">
+                  <AlertDescription>{alertMessage}</AlertDescription>
+                </Alert>
+              )}
 
-            <div className="space-y-4 w-full py-4 pb-8">
-              <div className="border-b-[1px] border-neutral-800 pb-8 w-full">
-                <ul className="flex flex-wrap gap-6 w-full">
-                  {team.members && team.members.map((member, index) => {
-                    console.log(member.pfp)
-                    return (
-                      <li key={index} className="flex items-center space-x-2 w-full lg:w-[35%] lg:bg-neutral-900 lg:rounded-lg lg:border-[1px] lg:border-neutral-600 lg:p-4">
-                        <Avatar>
-                          <AvatarImage src={member.pfp} height={10} width={10} />
-                          <AvatarFallback className="text-black">{member.name.charAt(0).toUpperCase()}</AvatarFallback>
-                        </Avatar>
-                        <div className="flex flex-col">
-                          <span className="text-white text-[16px]">{member.name}</span>
-                          <span className="text-white text-[12px] opacity-60">{member.roll_number}</span>
-                        </div>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-              <div className="flex justify-between items-center">
-                <span className="font-semibold w-full">Payment Status: </span>
-                <span className="font-semibold w-full text-yellow-500 text-right">{team.payment_status}</span>
-              </div>
-              <div className="flex justify-between items-center">
-                <span className="font-semibold">Team ID:</span>
-                <div className="flex items-center">
-                  <span>{team.team_id}</span>
-                  <Button
-                    onClick={() => copyToClipboard(team.team_id)}
-                    className="ml-2 p-1 bg-transparent hover:bg-gray-700 rounded-full"
-                  >
-                    <Copy className="h-4 w-4" />
-                  </Button>
+              <div className="space-y-4 w-full py-4 pb-8">
+                <div className="pb-8 w-full">
+                  <ul className="flex flex-wrap gap-6 w-full">
+                    {team.members && team.members.map((member, index) => {
+                      console.log(member.pfp)
+                      return (
+                        <li key={index} className="flex items-center space-x-2 w-full lg:bg-neutral-900 lg:rounded-lg lg:border-[1px] lg:border-neutral-600 lg:p-4">
+                          <Avatar>
+                            <AvatarImage src={member.pfp} height={10} width={10} />
+                            <AvatarFallback className="text-black">{member.name.charAt(0).toUpperCase()}</AvatarFallback>
+                          </Avatar>
+                          <div className="flex flex-col">
+                            <span className="text-white text-[16px]">{member.name}</span>
+                            <span className="text-white text-[12px] opacity-60">{member.roll_number}</span>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </main>
+        <div className="px-8 w-full py-4 border-t-[1px] border-neutral-800">
+          {team.members.length >= 4 && team.members.length <= 5 && (team.payment_status === "Pending" || team.payment_status === "Failed") ?
+            <div className="text-green-400 py-4">Minimum criteria of members met. Please proceed with payment.</div>
+            : team.members.length < 4 || team.members.length > 5 ?
+              <div className="text-yellow-400">Minimum criteria of members per team not met. Invite your teammates to make the payment.</div>
+              : <></>
+          }
+          <div className="flex justify-between items-center">
+            <span className="font-semibold w-full">Payment Status: </span>
+            <span className="font-semibold w-full text-right">{team.payment_status}</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-semibold">Team ID:</span>
+            <div className="flex items-center">
+              <span>{team.team_id}</span>
+              <Button
+                onClick={() => copyToClipboard(team.team_id)}
+                className="ml-2 p-1 bg-transparent hover:bg-gray-700 rounded-full"
+              >
+                <Copy className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
