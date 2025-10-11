57836734c4e426666b381e91c784161555915523
24f2000638-cmd <24f2000638@ds.study.iitm.ac.in>
2025-10-11 23:26:22 +0530

    small fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -38,7 +38,7 @@ import {
 } from "@/components/ui/dialog";
 import { VscDebugRestart } from "react-icons/vsc";
 import { toast } from "sonner";
-import { set } from "animejs";
+
 // import { set } from "animejs";
 
 //import { set } from "animejs";
